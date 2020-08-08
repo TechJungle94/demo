e2916c96cc8f9fdd4ac969992051c8ea0005cdbc
yjiao47 <yjiao47@gatech.edu>
2020-08-07 22:08:51 -0400

    add sources configuration

--- src/main/java/com/example/demo/Controller/HelloController.java
+++ src/main/java/com/example/demo/Controller/HelloController.java
@@ -1,11 +1,17 @@
 package com.example.demo.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.pojo.Resource;
+
 @RestController
 public class HelloController {
-    
+    @Autowired
+    private Resource resource;
+
+
     @RequestMapping("/hello")
     public Object hello() {
         return "hello springboot~~~~~~~";
