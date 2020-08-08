246813ed9ba95202c3c84803ce5ca7588268ae3c
yjiao47 <yjiao47@gatech.edu>
2020-08-07 22:52:36 -0400

    finish configuration of resource

--- src/main/java/com/example/demo/Controller/HelloController.java
+++ src/main/java/com/example/demo/Controller/HelloController.java
@@ -1,9 +1,11 @@
 package com.example.demo.Controller;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.pojo.JsonResult;
 import com.example.demo.pojo.Resource;
 
 @RestController
@@ -11,9 +13,16 @@ public class HelloController {
     @Autowired
     private Resource resource;
 
-
+    
     @RequestMapping("/hello")
     public Object hello() {
         return "hello springboot~~~~~~~";
+    }
+
+    @RequestMapping("/getResource")
+    public JsonResult getResource() {
+        Resource bean = new Resource();
+        BeanUtils.copyProperties(resource, bean);
+        return JsonResult.ok(bean);
     }
 }
